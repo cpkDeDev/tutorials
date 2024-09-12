34f1d38eb01bf2c8da279e7205e639f059601035
Peter Pulter <peter.pulter@cpk-company.de>
2024-09-12 15:13:40 +0200

    Tutorial Chapter 5 till Reserved Fields

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -8,6 +8,11 @@ Created on 12.09.2024
     'depends': [
         'base'
     ],
+    'data':[
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_property_menus.xml'
+        ],
     'installable': True,
     'application': True
 }
